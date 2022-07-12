543f12319d3e1581825c079efb740cdece14a765
Jnguye56 <108108666+Jnguye56@users.noreply.github.com>
2022-07-11 21:21:05 -0500

    Add files via upload

---
+++ src/controller/ShapeList.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.util.List;
+
+import model.CreateShape;
+
+public class ShapeList {
+	
+	private List<CreateShape> shapeList;
+	
+	public ShapeList(List<CreateShape> shapeList) {
+		this.shapeList = shapeList; 
+	}
+	
+	public void addShape(CreateShape shape) {
+		shapeList.add(shape); 		
+	}
+	
+	public void removeShape(CreateShape shape) {
+		shapeList.remove(shape);
+	}
+	
+	public List<CreateShape> getList() {
+		return shapeList;
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,9 @@ package main;
 
 import controller.IJPaintController;
 import controller.JPaintController;
+import controller.MouseHandler;
+import controller.ShapeList;
+import model.CreateShape;
 import model.ShapeColor;
 import model.ShapeType;
 import model.persistence.ApplicationState;
@@ -15,38 +18,32 @@ import view.interfaces.IUiModule;
 import java.awt.*;
 import java.util.Collection;
 import java.util.EnumMap;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args){
+    	
         PaintCanvasBase paintCanvas = new PaintCanvas();
         IGuiWindow guiWindow = new GuiWindow(paintCanvas);
         IUiModule uiModule = new Gui(guiWindow);
         ApplicationState appState = new ApplicationState(uiModule);
         IJPaintController controller = new JPaintController(uiModule, appState);
+        
+        List<CreateShape> shapeList = new ArrayList<CreateShape>();
+        final ShapeList ShapeList = new ShapeList(shapeList);
+        
+        MouseHandler handler = new MouseHandler(paintCanvas,appState, ShapeList);
+        paintCanvas.addMouseListener(handler);
+        
         controller.setup();
-
-        // For example purposes only; remove all lines below from your final project.
-
+        
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        // Filled in rectangle
-        Graphics2D graphics2d = paintCanvas.getGraphics2D();
-        graphics2d.setColor(Color.GREEN);
-        graphics2d.fillRect(12, 13, 200, 400);
-
-        // Outlined rectangle
-        graphics2d.setStroke(new BasicStroke(5));
-        graphics2d.setColor(Color.BLUE);
-        graphics2d.drawRect(12, 13, 200, 400);
-
-        // Selected Shape
-        Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
-        graphics2d.setStroke(stroke);
-        graphics2d.setColor(Color.BLACK);
-        graphics2d.drawRect(7, 8, 210, 410);
+        
     }
 }
